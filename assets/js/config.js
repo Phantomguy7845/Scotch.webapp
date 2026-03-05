cb994260eb8a49cab42db14d8b867d930fc8c2f0
Phantomguy7845 <Phantomguy7845@users.noreply.github.com>
2026-03-05 20:00:56 +0700

    Update Apps Script web app URL in config

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   apiBaseUrl:
-    "https://script.google.com/macros/s/AKfycbyEba-N2MAGvxQ0m42pG1DccJgcF6psKj9aTNFqvVAvmjP5ZfX_z0Fhp5NNmg48ggpmag/exec",
+    "https://script.google.com/macros/s/AKfycbx9f--paFvDXAblCdhEh0F7MDOSX3u8oz-HATCLe_-Yzv0IWAcTTMKJMRgzJo_6Xqev/exec",
   adminKey: "",
   appName: "Scotch webapps1",
 };
